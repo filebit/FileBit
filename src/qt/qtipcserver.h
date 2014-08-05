404686703b964fdaaf937bab947f10d8dd5445bb
FileBit <filebit07@hotmail.com>
2014-08-06 01:13:16 +0530

    First Commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define FileBit-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "FileBitURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
